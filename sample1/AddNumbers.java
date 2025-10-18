b7f20fd619030d3c60d47fd5e7b606f21a70ca55
chickenairsold <msathish@madisoncollege.edu>
2025-10-18 02:53:59 +0000

    first commit

---
+++ sample1/AddNumbers.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class AddNumbers{
+    public static void main(String args[]){
+        Scanner scnr = new Scanner(System.in);
+        int a=0;
+        int b=0;
+    int c;
+    int d; // difference (a - b)
+
+        a = scnr.nextInt();
+        b = scnr.nextInt();
+
+        if(a <= b){
+            System.out.println("a is less than b ");
+        }
+        
+
+    c = a + b;
+    System.out.println("The value of c ==>" + c);
+
+    d = a - b;
+    System.out.println("The value of subtraction ==>" + d);
+
+    int[] arr = new int[]{a, b, c, d};
+
+    // Print elements using an indexed for-loop
+    System.out.println("Printing with indexed for-loop:");
+    for (int i = 1; i < arr.length; i++) {
+        System.out.println("arr[" + i + "] = " + arr[i]);
+    }
+
+
+    scnr.close();
+    }
+    
+}
